e270d090ce0e089fe500c48724867058d5130c43
Hansel Wei <hello@hanselwei.dev>
2022-12-06 02:14:05 -0500

    Update Bitmoji.vue

--- components/Bitmoji.vue
+++ components/Bitmoji.vue
@@ -8,8 +8,11 @@
 .bitmojiAni {
   border-radius: 350px;
   box-sizing: border-box;
+ 
+  background-image: linear-gradient(45deg, white 25%, transparent 25.5%, transparent 50%, white 50.5%, white 75%, transparent 75.5%, transparent);
+  background-size: 40px 40px;
 
-  border: 7px solid #cfd8dc;
+  border: 7px red;
   display: inline-block;
   animation: turn 0.5s linear backwards 1.5s;
   transform: rotateY(720deg);
