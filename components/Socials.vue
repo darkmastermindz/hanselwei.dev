295539038f90d26a7a432b8471de4f4cffbdaad9
Hansel Wei <hello@hanselwei.dev>
2022-12-06 01:24:11 -0500

    Update Socials.vue

--- components/Socials.vue
+++ components/Socials.vue
@@ -4,24 +4,27 @@
           class="relative z-0 justify-center px-8  shadow-sm rounded-md font-bold md:antialiased sm:subpixel-antialiased">
           <a href="https://github.com/darkmastermindz" target="_blank" 
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-grey-700 relative inline-flex items-center px-8 py-2 rounded-l-md border">Github</a>
-          <a href="https://hansel.run/darkmastermindz" target="_blank" 
+         
+          <a href="https://hansel.run/mastodon" target="_blank" 
+            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib bg-[#563acc] text-white relative inline-flex items-center px-8 py-2 rounded-l-md border">Twitter</a>
+          <a href="https://hansel.run/tw" target="_blank" 
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib bg-[#1da1f2] text-white relative inline-flex items-center px-8 py-2 rounded-l-md border">Twitter</a>
           <a href="https://snapchat.com/add/darkmastermindz" target="_blank" tabindex="7"
-            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-grey bg-yellow-300 relative inline-flex items-center px-8 py-2 rounded-l-md border">Snapchat</a>
+            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-[#ffffff] bg-[#fffc00] relative inline-flex items-center px-8 py-2 rounded-l-md border">Snapchat</a>
           <a href="https://m.me/hanselwei" target="_blank"
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-blue-600 relative inline-flex items-center px-8 py-2 rounded-l-md border">Messenger</a>
           <a href="https://hansel.rocks" target="_blank" 
-            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-red relative inline-flex items-center px-8 py-2 rounded-l-md border">Instagram</a>
+            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-[#f46f30] relative inline-flex items-center px-8 py-2 rounded-l-md border">Instagram</a>
           <a href="https://polywork.com/darkmastermindz" target="_blank"
-            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-purple-700 relative inline-flex items-center px-8 py-2 rounded-l-md border">Polywork</a>
+            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-[#582BE8] relative inline-flex items-center px-8 py-2 rounded-l-md border">Polywork</a>
           <a href="https://linkedin.com/in/hanselwei" target="_blank" tabindex="11"
-            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-blue-700 relative inline-flex items-center px-8 py-2 rounded-l-md border">Linkedin</a>
+            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-[#0a66c2] relative inline-flex items-center px-8 py-2 rounded-l-md border">Linkedin</a>
           <a href="mailto:Hansel Wei (hanselwei.dev) <hello+hansel@hanselwei.dev>" target="_blank" 
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-green relative inline-flex items-center px-4 py-2 rounded-l-md border">Send
             Email</a>
           <br>
           <a href="https://hansel.run/connect" target="_blank" tabindex="12"
-            class="visible md:invisible f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-green-300 relative inline-flex items-center px-4 py-2 rounded-l-md border">💾 Save to Contacts</a>
+            class="visible md:invisible f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-[#00b489] relative inline-flex items-center px-4 py-2 rounded-l-md border">💾 Save to Contacts</a>
         </span>
     </div>
 </template>
