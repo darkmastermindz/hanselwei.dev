727977a7aed8f8f53c41599198724bace2470b87
Hansel Wei <hello@hanselwei.dev>
2022-12-06 02:25:35 -0500

    Update Bitmoji.vue

--- components/Bitmoji.vue
+++ components/Bitmoji.vue
@@ -9,7 +9,7 @@
   border-radius: 350px;
   box-sizing: border-box;
  
-  background-image: linear-gradient(45deg, white 25%, transparent 25.5%, transparent 50%, white 50.5%, white 75%, transparent 75.5%, transparent);
+  background-image: linear-gradient(45deg,#fff 25%,red, 25.5%,lawngreen 50%,#fff 50.5%,white 75%,transparent 75.5%,red);
   background-size: 40px 40px;
 
   border: 7px red;
