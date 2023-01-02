ea6f3f175e88b1021707ba3cd468b39b3c01c5e6
Hansel Wei <hello@hanselwei.dev>
2023-01-02 19:48:33 +0000

    Update Profile Header Background Color to Rebecca Purple, Royal Purple, and Bud Green

--- components/Bitmoji.vue
+++ components/Bitmoji.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bitmojiAni">
-    <img svg-inline class="icon" src="~/assets/hanselbitmoji.svg" alt="example" />
+    <img svg-inline class="icon" src="~/assets/hanselbitmoji.svg" alt="hansel's profile picture" />
   </div>
 </template>
 
@@ -9,17 +9,17 @@
   border-radius: 350px;
   box-sizing: border-box;
  
-  background-image: linear-gradient(4deg,#fff 35%,red,25.5%,#7cfc00 50%,#fff 50.5%,#fff 75%,red 75.5%,yellowgreen);
+  background-image: linear-gradient(11893deg,#fff 22%,#653496,23%,#7cfc00 77%,#fff 50.5%,#fff 77%,#653496 77%,#7B52AE);
   background-size: 40px 40px;
   
-  animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1);
 
   @keyframes pulse {
     0%, 100% {
       opacity: 1;
     }
     50% {
-      opacity: .5;
+      opacity: .77;
     }
   }
 
