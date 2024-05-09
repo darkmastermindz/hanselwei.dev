430a2a3d356258a59c975e629fa54d37564a69b5
Hansel Wei <hello@hanselwei.dev>
2024-05-09 15:22:57 -0400

    Update Socials.vue
    Update spacing

--- components/Socials.vue
+++ components/Socials.vue
@@ -2,9 +2,10 @@
     <div class="justify-center md:px-16">
         <div
           class="relative z-0 justify-center px-8  shadow-sm rounded-md font-bold md:antialiased sm:subpixel-antialiased">
-   
+          <br><br>
           <a href="https://hansel.run/connect" target="_blank" 
             class="visible md:invisible f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-[#00b489] relative inline-flex items-center px-4 py-2 rounded-l-md border">Open  Apple Contact Card ✨</a>
+          <br><br>
          <!-- <br><br>
               <a href="https://gist.github.com/darkmastermindz/8df2a44f4743dcef39efffcded7b2c28#file-raleigh_nc_food_recs-md" target="_blank" 
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-[#00b489] relative inline-flex items-center px-4 py-2 rounded-l-md border">Raleigh, NC Food/Entertainment Recs around ATO2023</a>
@@ -13,8 +14,6 @@
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib text-white bg-grey-700 relative inline-flex items-center px-8 py-2 rounded-l-md border">Github</a>
           <a href="https://hansel.run/mastodon" target="_blank" 
             class="f6 grow no-underline br-pill ph3 pv2 mb2 dib bg-[#563acc] text-white relative inline-flex items-center px-8 py-2 rounded-l-md border">Mastodon</a>
-          <a href="https://hansel.run/tw" target="_blank" 
-            class="f6 grow no-underline br-pill ph3 pv2 mb2 dib bg-[#1da1f2] text-white relative inline-flex items-center px-8 py-2 rounded-l-md border">Twitter</a>
           <a href="https://snapchat.com/add/darkmastermindz" target="_blank"
             class=" f6 grow no-underline br-pill ph3 pv2 mb2 dib text-slate-600 bg-[#fffc00] relative inline-flex items-center px-8 py-2 rounded-l-md border">Snapchat</a>
           <a href="https://m.me/hanselwei" target="_blank"
